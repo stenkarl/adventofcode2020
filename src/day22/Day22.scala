5aa2fc0af5ce75188ac06ebdf9dde0eba4b0a08f
stenkarl <sten_karl@yahoo.com>
2020-12-23 16:03:19 -0600

    Day 22 part 2

--- src/day22/Day22.scala
+++ src/day22/Day22.scala
@@ -1,5 +1,6 @@
 package day22
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
@@ -14,7 +15,7 @@ object Day22 {
     val p1 = input.slice(1, blankIndex).map(_.toInt)
     val p2 = input.slice(blankIndex + 2, input.size).map(_.toInt)
 
-    println(part1(p1.to[ListBuffer], p2.to[ListBuffer]))
+    println(part2(p1.to[ListBuffer], p2.to[ListBuffer]))
   }
 
   def part1(p1:ListBuffer[Int], p2:ListBuffer[Int]):Int = {
@@ -23,7 +24,6 @@ object Day22 {
 
     var round = 1
     while (p1.nonEmpty && p2.nonEmpty) {
-    //for (i <- 1 to 5) {
       val card1 = p1.remove(0)
       val card2 = p2.remove(0)
       if (card1 > card2) {
@@ -45,6 +45,67 @@ object Day22 {
     println(ret)
 
     ret.sum
+  }
+
+  def part2(p1:ListBuffer[Int], p2:ListBuffer[Int]):Int = {
+    val p1Winner = subgame(p1, p2, 1)
+
+    val winner = if (p1Winner) p1 else p2
+
+    println(s"Winner's deck $winner")
+
+    val ret = winner.reverse.zipWithIndex.map(it => it._1 * (it._2 + 1))
+
+    println(ret)
+
+    ret.sum
+  }
+
+  def subgame(p1:ListBuffer[Int], p2:ListBuffer[Int], num:Int):Boolean = {
+    //println()
+    //println(s"=== Game $num with $p1 $p2 ===")
+    val set = mutable.Set[(String, String)]()
+
+    var round = 1
+    while (p1.nonEmpty && p2.nonEmpty) {
+      val newConfig = (p1.mkString(","), p2.mkString(","))
+      if (set.contains(newConfig)) {
+        println(s"seen this config before: $newConfig")
+        return true
+      }
+      set.add(newConfig)
+      val card1 = p1.remove(0)
+      val card2 = p2.remove(0)
+      if (card1 <= p1.size && card2 <= p2.size) {
+        //println(s"spawning new game $card1 ${p1.size}, $card2 ${p2.size}")
+        val newp1 = p1.slice(0, card1)
+        val newp2 = p2.slice(0, card2)
+        val p1Winner = subgame(newp1, newp2, num + 1)
+        //println(s"meanwhile back to game $num")
+        if (p1Winner) {
+          p1.append(card1)
+          p1.append(card2)
+        } else {
+          p2.append(card2)
+          p2.append(card1)
+        }
+      } else if (card1 > card2) {
+        //println(s"Player 1 wins $card1 and $card2")
+        p1.append(card1)
+        p1.append(card2)
+      } else {
+        //println(s"Player 2 wins $card1 and $card2")
+        p2.append(card2)
+        p2.append(card1)
+      }
+      //println(s"End of round $round p1:$p1 and p2:$p2")
+      round = round + 1
+    }
+    val winner = p1.nonEmpty
+    //println(s"Player ${if (winner) 1 else 2} wins game $num")
+    //println(s"=== Game $num ends ===")
+    //println()
+    winner
   }
 
 }
